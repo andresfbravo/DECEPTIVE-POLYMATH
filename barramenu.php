d383d678705682824090034f786e5b93463fb284
Juan Pablo Ospina Bustamante <juanpablo.ospina@utp.edu.co>
2018-05-23 23:15:26 -0500

    Add files via upload

---
+++ barramenu.php
@@ -0,0 +1,8 @@
+
+		<div class="navbar-collapse collapse">
+			<div class="container">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Inicio <b class="caret"></b></a></li>
+				</ul>
+			</div>
+		</div>		
