bb171fea656ea83bbf41b91a3e4e6d981db463b0
Juan Pablo Ospina Bustamante <juanpablo.ospina@utp.edu.co>
2018-05-28 15:59:26 -0500

    Update barramenuprof.php

--- DPapp/VistasUsuarios/barramenuprof.php
+++ DPapp/VistasUsuarios/barramenuprof.php
@@ -4,6 +4,8 @@
 		<div class="container">
 			<ul class="nav navbar-nav">
 				<li><a href="/deceptive-polymath/DPapp/VistasUsuarios/Vistaprofesor.php">Inicio</a></li>
+				<li><a href="#">Revisar preguntas</a><li>
+				<li><a href="/deceptive-polymath/DPapp/Generarexamen.php">Generar Examen</a></li>
 			</ul>
 			<ul class = "nav navbar-nav navbar-right">
 				<button type="button" style = "background-color:#FAFAFA" class="btn btn-primary" id ="login"><a href = "/deceptive-polymath/DPapp/logout.php"> Log Out</a></button>
